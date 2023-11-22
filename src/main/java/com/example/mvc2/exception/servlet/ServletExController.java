910495d0d6343ba02533635d383af73d6961be80
LJW <dementor0711@gmail.com>
2023-11-22 17:47:21 +0900

    ~서블릿 예외 처리 - 오류 페이지 작동 원리

---
+++ src/main/java/com/example/mvc2/exception/servlet/ServletExController.java
@@ -0,0 +1,39 @@
+package com.example.mvc2.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+/**
+ * packageName    : com.example.mvc2.exception.servlet
+ * fileName       : ServletExController
+ * author         : Mr.Lee
+ * date           : 2023-11-22
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-11-22        Mr.Lee      최초 생성
+ */
+
+@Slf4j
+@Controller
+public class ServletExController {
+
+    @GetMapping("/error-ex")
+    public void errorEx(){
+        throw new RuntimeException("예외 발생");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!");
+    }
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+}
